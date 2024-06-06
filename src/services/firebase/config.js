302bc015a88852fe0f386fcf8a7f8d60dc566266
kirbyramos <ramos.christiankirby@gmail.com>
2024-06-07 01:45:15 +0800

    first commit

---
+++ src/services/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCyHiJBljNFCP6b2Mf4zQMcx0gRuXWDgBs",
+  authDomain: "todolist-ce720.firebaseapp.com",
+  projectId: "todolist-ce720",
+  storageBucket: "todolist-ce720.appspot.com",
+  messagingSenderId: "246284150449",
+  appId: "1:246284150449:web:64d0ba4786e0df03942490",
+  measurementId: "G-W4S062102Z"
+};
+
+export default firebaseConfig;
